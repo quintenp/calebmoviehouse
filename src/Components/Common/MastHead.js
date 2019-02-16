e63ad2800e7b9a5dabe2c5d5236436811cb9bf4a
Quinten <Quinten.pohl@gmail.com>
2019-02-16 21:26:16 +0000

    cleanup masthead

--- src/Components/Common/MastHead.js
+++ src/Components/Common/MastHead.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar';
 
-class MastHead extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Calebs Movie House</h1>
-                <NavBar />
-            </div>
-        );
-    }
-}
+const MastHead = () => {
+    return (
+        <div>
+            <h1>Calebs Movie House</h1>
+            <NavBar />
+        </div>
+    );
+};
 
 export default MastHead;
